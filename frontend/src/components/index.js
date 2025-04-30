48ae07e521d9937c81561afa11c68dbe597cef79
anandsub-zuper <anandsub@zuper.co>
2025-04-29 20:21:10 -0700

    Create index.js

---
+++ frontend/src/components/index.js
@@ -0,0 +1,11 @@
+// src/components/index.js
+// Export all components for easier imports
+
+// Main components
+export { default as RoofAnalyzer } from './RoofAnalyzer';
+export { default as EnhancedResultsDisplay } from './EnhancedResultsDisplay';
+export { default as LoadingSpinner } from './LoadingSpinner';
+
+// Utility components
+export { default as CostEstimation } from './CostEstimation';
+export { default as DamageAssessment } from './DamageAssessment';
